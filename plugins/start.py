4fcf84dbd4169ab495b9992feaaaf64b299a9d93
devilkky <89154959+devilkky@users.noreply.github.com>
2022-02-24 16:08:01 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -12,8 +12,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/theimdbshow_supportgroup") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://t.me/theIMDbshow") ]  ]))
+	 [[ InlineKeyboardButton("Updates channel 🔗",url="https://t.me/theimdbshow_supportgroup") ], 
+	[InlineKeyboardButton("Support group 🧐", url="https://t.me/theIMDbshow") ]  ]))
 
 
 
